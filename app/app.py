c0f19e652a3fe1718e9f3e41bffb0c9369b2f486
syamk <syamk@qburst.com>
2019-03-29 14:41:37 +0530

    Updated python script with elasticsearch module

--- app/app.py
+++ app/app.py
@@ -2,15 +2,35 @@ from flask import Flask
 from elasticsearch import Elasticsearch
 from flask import jsonify
 import logging
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    es = Elasticsearch()
-    #data = es.search(index='school'])
-    #print(data)
-    return 'Flask Docker'
+    config = [{'host': 'elasticsearch', 'port': '9200'}]
+    es = Elasticsearch(config)
+    if not es.ping():
+        print('Elastic not available')
+        raise Exception('Cannot initialize elastic search')
+
+    try:
+        query_type  = request.args.get('q')
+        if query_type=='all':
+            fields =[]
+        else:
+            fields = ['hits.hits._id', 'hits.hits._source.log_type', 'hits.hits._source.message']
+      
+        data = es.search(index='logs*', filter_path=fields)
+    except Exception as e:
+        return str(e)
+    
+   
+    
+    if data:
+        return jsonify(data)
+
+    return 'No results'
 
 if __name__ == '__main__':
     logging.basicConfig(filename='logs/error_log',level=logging.DEBUG)
